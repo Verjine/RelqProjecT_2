eef5ca7879ac66bff721f74fab3e025573aaf84b
Verjine <verjine.pipoyan@gmail.com>
2022-08-12 11:02:18 +0400

    fix

---
+++ main.js
@@ -0,0 +1,25 @@
+const days = document.querySelectorAll('days');
+const hours = document.querySelectorAll('hours');
+const minutes = document.querySelectorAll('minutes');
+const seconds = document.querySelectorAll('seconds');
+
+const currentYear = new Date().getFullYear;
+const newYearTime = new Date(`November 15 $ {currentYear+1} 00:00:00`);
+
+
+function updateCountdown() {
+    let currentTime = new Date()
+    let diff = newYearTime - currentTime
+
+    var d = Math.floor(diff / (1000 * 60 * 60 * 24));
+    var h = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var m = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    var s = Math.floor((diff % (1000 * 60)) / 1000);
+
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds, innerHTML = s < 10 ? '0' + s : s;
+}
+
+setInterval(updateCountdown, 1000)
